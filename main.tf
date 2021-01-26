413806726f53553f61f10391af1f01c61830da8f
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-01-25 16:25:53 -0800

    I can't even remember really, but, sure, whatever

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ resource "null_resource" "randomm" {
     username = var.username
   }
   # only changes when configuration edited, or when username changes.
-  provisioner "local-exec" {
-    command = "curl https://beyondgrep.com/ack-v3.1.1 > ./ack && chmod 0755 ./ack"
-  }
-  provisioner "local-exec" {
-    command = "./ack randomm"
-  }
+  # provisioner "local-exec" {
+  #   command = "curl https://beyondgrep.com/ack-v3.1.1 > ./ack && chmod 0755 ./ack"
+  # }
+  # provisioner "local-exec" {
+  #   command = "./ack randomm"
+  # }
 }
 
 output "random" {
@@ -25,22 +25,22 @@ output "username" {
   value = "Username is ${var.username}. Extra text."
 }
 
-data "terraform_remote_state" "dev" {
-  backend = "remote"
-  workspace = "dev"
-  config = {
-    organization = "nicktech"
-    workspaces = {
-      # name = "minimum-dev"
-      prefix = "minimum-"
-    }
-  }
-}
+# data "terraform_remote_state" "dev" {
+#   backend = "remote"
+#   workspace = "dev"
+#   config = {
+#     organization = "nicktech"
+#     workspaces = {
+#       # name = "minimum-dev"
+#       prefix = "minimum-"
+#     }
+#   }
+# }
 
-output "username-dev" {
-  value = data.terraform_remote_state.dev.outputs.username
-}
+# output "username-dev" {
+#   value = data.terraform_remote_state.dev.outputs.username
+# }
 
-output "all-dev" {
-  value = data.terraform_remote_state.dev.outputs
-}
+# output "all-dev" {
+#   value = data.terraform_remote_state.dev.outputs
+# }
