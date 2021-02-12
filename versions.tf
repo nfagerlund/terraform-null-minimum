2efef991ffbc5e811912a128e6058c16d09d9694
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-02-12 12:39:50 -0800

    fmt versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0.0"
     }
     random = {
