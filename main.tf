ad1fa414790ea9c3051be18a660945e0de226eb1
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-05-20 15:44:35 -0700

    remove unused state access

--- main.tf
+++ main.tf
@@ -15,22 +15,3 @@ output "username" {
   value = "Username is ${var.username}. Extra text???"
 }
 
-# data "terraform_remote_state" "dev" {
-#   backend = "remote"
-#   workspace = "dev"
-#   config = {
-#     organization = "nicktech"
-#     workspaces = {
-#       # name = "minimum-dev"
-#       prefix = "minimum-"
-#     }
-#   }
-# }
-
-# output "username-dev" {
-#   value = data.terraform_remote_state.dev.outputs.username
-# }
-
-# output "all-dev" {
-#   value = data.terraform_remote_state.dev.outputs
-# }
